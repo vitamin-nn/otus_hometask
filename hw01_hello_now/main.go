5d10d6fe62251cca5129acd177a561657a7d5f4b
Vitaly Dudov <v.dudov@copybet.com>
2020-03-04 21:36:17 +0200

    feat: exact and local times

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	localTime := time.Now()
+	fmt.Printf("current time: %s\n", localTime.String())
+	fmt.Printf("exact time: %s\n", ntpTime.String())
 }
