ca4fb1017041af60cd7a26646029752d2e988da6
Vitaly Dudov <v.dudov@copybet.com>
2020-03-13 19:40:45 +0200

    feat: hw02

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,31 @@ package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here
-	return "", nil
+func Unpack(s string) (string, error) {
+	var b strings.Builder
+	var prev rune
+
+	for i, curr := range s {
+		if unicode.IsDigit(curr) {
+			if unicode.IsDigit(prev) || i == 0 {
+				return "", ErrInvalidString
+			}
+			//rep, _ := strconv.Atoi(string(curr))
+			rep := int(curr - '0')
+			w := strings.Repeat(string(prev), rep-1)
+			b.WriteString(w)
+		} else {
+			b.WriteRune(curr)
+		}
+		prev = curr
+	}
+	result := b.String()
+	return result, nil
 }
