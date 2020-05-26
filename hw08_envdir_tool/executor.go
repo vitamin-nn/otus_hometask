63a7eec1f24a3dca28fdbdb3238b189c872368ec
vitamin-nn <dudovv@mail.ru>
2020-05-26 21:35:46 +0300

    hw08 is completed (#8)

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,38 @@
 package main
 
-// RunCmd runs a command + arguments (cmd) with environment variables from env
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+// RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here
-	return
+	var args []string
+	if len(cmd) > 1 {
+		args = cmd[1:]
+	}
+	command := cmd[0]
+	execCmd := exec.Command(command, args...)
+	execCmd.Env = prepareEnv(env)
+	execCmd.Stdin = os.Stdin
+	execCmd.Stdout = os.Stdout
+	execCmd.Stderr = os.Stderr
+
+	err := execCmd.Run()
+	if err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			return exitError.ExitCode()
+		}
+		return defaultExitErrCode
+	}
+	return 0
+}
+
+func prepareEnv(env Environment) []string {
+	envList := os.Environ()
+	for k, v := range env {
+		envList = append(envList, fmt.Sprintf("%s=%s", k, v))
+	}
+	return envList
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,85 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("cannot run cmd", func(t *testing.T) {
+		cmd := []string{"/dev/null", "1", "2"}
+		env := Environment{"TEST": "VALUE"}
+		code := RunCmd(cmd, env)
+		require.Equal(t, defaultExitErrCode, code)
+	})
+
+	t.Run("cmd error code return", func(t *testing.T) {
+		script := `#!/bin/sh
+exit 1
+`
+		tmpFileName, err := makeShFile(script)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer os.Remove(tmpFileName)
+
+		cmd := []string{
+			"/bin/sh",
+			tmpFileName,
+		}
+		env := Environment{}
+		code := RunCmd(cmd, env)
+		require.Equal(t, 1, code)
+	})
+
+	t.Run("cmd writes to Stdout", func(t *testing.T) {
+		script := `#!/bin/sh
+printf "out"
+`
+		tmpFileName, err := makeShFile(script)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer os.Remove(tmpFileName)
+
+		rescueStdout := os.Stdout
+		r, w, err := os.Pipe()
+		if err != nil {
+			t.Fatal(err)
+		}
+		os.Stdout = w
+		cmd := []string{
+			"/bin/sh",
+			tmpFileName,
+		}
+		env := Environment{}
+		_ = RunCmd(cmd, env)
+		w.Close()
+		out, err := ioutil.ReadAll(r)
+		if err != nil {
+			t.Fatal(err)
+		}
+		os.Stdout = rescueStdout
+		require.Equal(t, "out", string(out))
+	})
+}
+func makeShFile(content string) (string, error) {
+	tmpfile, err := ioutil.TempFile("", "hw08_test_file_*.sh")
+	if err != nil {
+		return "", err
+	}
+	tmpFileName := tmpfile.Name()
+
+	if _, err := tmpfile.Write([]byte(content)); err != nil {
+		os.Remove(tmpFileName)
+		return "", err
+	}
+	if err := tmpfile.Close(); err != nil {
+		os.Remove(tmpFileName)
+		return "", err
+	}
+	return tmpFileName, nil
 }
